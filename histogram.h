0513d3d9476c887de886d83d9a61c768577be9c2
Shumilov Egor <102813675+ShumilovES@users.noreply.github.com>
2022-06-01 16:23:53 +0300

    Изменение в структуру input_numbers

--- histogram.h
+++ histogram.h
@@ -5,14 +5,5 @@
 
 using namespace std;
 
-vector<double> input_numbers(istream& in,  size_t count);
-
-void show_bin(vector<size_t> bins, const double max_bin, const double ratio);
-
-size_t find_max(vector<size_t> bins);
-
 void find_minmax( const vector<double> numbers, double& min, double& max);
 
-double find_ratio(size_t max_bin, const size_t bin_heght, const size_t Height, const size_t Red_line);
-
-vector<size_t> make_histogram(const vector<double>& numbers, size_t bin_count, double min, double max);
