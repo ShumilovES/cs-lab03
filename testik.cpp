bab27ab3f501832193d21d981eba543392d603ff
Shumilov Egor <102813675+ShumilovES@users.noreply.github.com>
2022-06-06 10:25:23 +0300

    Revert last commit

--- testik.cpp
+++ testik.cpp
@@ -6,12 +6,20 @@
 #include <curl/curl.h>
 #include <sstream>
 #define CURL_STATICLIB
+#include <cstdio>
+
 
 using namespace std;
 
 
 int main(int argc, char* argv[])
 {
+	const char* name = "Commander Shepard";
+	int year = 2154;
+	printf("%s was born in %d.\n", name, year); // Commander Shepard was born in 2154.
+	printf("n = %08x\n", 0x1234567); // 01234567
+	return 0;
+
 	Input input;
 	if (argc > 1)
 	{
@@ -35,6 +43,11 @@ int main(int argc, char* argv[])
 
 	show_histogram_svg(bins);
 
+	//char* curl_version();
+
+	cerr << "CURL: " << curl_version_info(CURLVERSION_NOW)->version << "\n" << "SSL: " << curl_version_info(CURLVERSION_NOW)->ssl_version;
+
 	return 1;
+
 }
 
